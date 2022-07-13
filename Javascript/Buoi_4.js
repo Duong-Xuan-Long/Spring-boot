a4f2e7a55d293130cfe672e0f610b24ee9d14fb8
Duong-Xuan-Long <linesco218@gmail.com>
2022-07-13 18:56:24 +0700

    bai13_7

---
+++ Javascript/Buoi_4.js
@@ -0,0 +1,7 @@
+const fruitBasket = {banana:1, cherry:2};
+
+// const count = fruitBasket.reduce( (tall, fruit) => {
+//   tall[fruit] = (tall[fruit] || 0) + 1 ;
+//   return tall;
+// } , {})
+console.log(fruitBasket.banana);
